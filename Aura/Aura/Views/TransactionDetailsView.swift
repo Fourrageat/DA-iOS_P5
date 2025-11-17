f5ef9569b63f12cfb204d3d78cdb84176dc0d38f
Fourrageat <baptiste.fourrageat@icloud.com>
2025-11-17 03:39:40 +0100

    add TransactionDetailView screen and add navigation to AccountDetailsView

---
+++ Aura/Aura/Views/TransactionDetailsView.swift
@@ -0,0 +1,21 @@
+//
+//  TransactionDetailsView.swift
+//  Aura
+//
+//  Created by Baptiste Fourrageat on 17/11/2025.
+//
+
+import SwiftUI
+
+struct TransactionDetailsView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, World!")
+        }
+        .toolbar(.hidden, for: .tabBar)
+    }
+}
+
+#Preview {
+    TransactionDetailsView()
+}

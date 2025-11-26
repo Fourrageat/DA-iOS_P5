1799dc11be466a2138f671be1907310dfaced503
Fourrageat <baptiste.fourrageat@icloud.com>
2025-11-26 11:00:33 +0100

    wip

--- Aura/Aura/Views/TransactionDetailsView.swift
+++ Aura/Aura/Views/TransactionDetailsView.swift
@@ -11,12 +11,11 @@ struct TransactionDetailsView: View {
     @ObservedObject var viewModel: AccountDetailViewModel
     
     var body: some View {
+        Text("All Transactions")
+            .font(.headline)
+            .padding([.horizontal])
         ScrollView {
             LazyVStack(alignment: .leading, spacing: 10) {
-                Text("All Transactions")
-                    .font(.headline)
-                    .padding([.horizontal])
-                
                 TransactionList(transactions: viewModel.recentTransactions)
             }
             .padding(.top, 10)
